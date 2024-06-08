9689ce1ad415002f238a06a60dbfa9d5a2fffffd
nisal <nisalsocials@gmail.com>
2024-06-09 01:43:49 +0530

    Initial commit

---
+++ carrental/src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2 className="note8">
+        Made w/ ❤️ by <a href="https://codepen.io/Mooncaque" target="_blank" rel="noopener noreferrer">Nisal Herath</a>
+      </h2> 
+    </footer>
+  );
+};
+
+export default Footer;
